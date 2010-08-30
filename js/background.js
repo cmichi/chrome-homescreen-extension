ed3c350b9830d17294402840fbf96add0a1213f2
cMichi <mich@elmueller.net>
2010-08-31 01:17:00 +0200

    pagination now properly working

--- js/background.js
+++ js/background.js
@@ -1,43 +1,39 @@
 globalTabRegistry = new Array();
 localStorage.recentlyClosedTabs = JSON.stringify(new Array());
-//alert(localStorage.recentlyClosedTabs);
 allNewTabs = new Array();
 
 function updateAllNewTabPages() {
 	// all opened 'new tab' pages have to be updated
 	// so that they show the latest closed tabs
-//	alert(allNewTabs + "!!!!!!!!!!!");
+
 	for (i = 0; i < allNewTabs.length; i++) {
-		if (allNewTabs[i] != null) {
-//			alert(allNewTabs[i] + "XXXX");
-//			alert('sending...');
-			chrome.tabs.sendRequest(allNewTabs[i]);
-			
-//			chrome.tabs.get(allNewTabs[i], function abcd(tab) { tab })
-		}
+		if (allNewTabs[i] != null)
+			chrome.tabs.sendRequest(allNewTabs[i], {});
 	}	
 }
 
-chrome.tabs.onRemoved.addListener(function (tabId) {
-	ob = JSON.parse(localStorage.recentlyClosedTabs);
-	ob[tabId] = globalTabRegistry[tabId];
-	localStorage.recentlyClosedTabs = JSON.stringify(ob);
+chrome.tabs.onRemoved.addListener(function (tabId) {	
+	tab = JSON.parse(globalTabRegistry[tabId]);
 	
-	updateAllNewTabPages();
+	if (tab["url"] != "chrome://newtab/") {
+		obj = JSON.parse(localStorage.recentlyClosedTabs);
+		// items have to be in order how they were close, 
+		// not how they have been created
+		obj.push(globalTabRegistry[tabId]); 
+		localStorage.recentlyClosedTabs = JSON.stringify(obj);
+
+		updateAllNewTabPages();
+	}
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)  {
 	ob =  {"title" : tab.title, "url" : tab.url};
 	globalTabRegistry[tab.id] = JSON.stringify(ob);
-//	allNewTabs[tab.id] = null;
+	allNewTabs[tab.id] = null;
 });
 
 
 chrome.tabs.onCreated.addListener(function (tab) {
 	allNewTabs.push(tab.id);
 	allNewTabs[tab.id] = tab.id;
-//	alert(allNewTabs + "====");
-	
-	//ob =  {"title" : tab.title, "url" : tab.url};
-	//globalTabRegistry[tab.id] = JSON.stringify(ob);
 });
--- js/newtab.js
+++ js/newtab.js
@@ -1,5 +1,4 @@
 maxEntriesPerPage = 7;
-thereIsANextPage = false;
 log = document.getElementById("log");
 currentPage = 0;
 ob = JSON.parse(localStorage.recentlyClosedTabs);
@@ -10,7 +9,6 @@ String.prototype.trunc = function(n) {
 
 
 function display(page) {
-    thereIsANextPage = false;
     if (page < 0) return;
     if (page == 0) {
         document.getElementById('previous_inactive').style.display = "block";
@@ -20,44 +18,25 @@ function display(page) {
         document.getElementById('previous_active').style.display = "block";
     }
 
-    count = 0;
-    liste = "";
-    c = 0;
-    onPage = 0;
-    currentPage = page;
 
-    //	for (var i = 0; i <10; i++) {
-    for (var i = ob.length; i > 0; i--) {
-        //		log.innerHTML = log.innerHTML + "<br />" + ob[i];
-        if (ob[i] != null) {
-            _ob = JSON.parse(ob[i]);
-
-            if (_ob.url != "chrome://newtab/") {
-                c++;
-                if (c > maxEntriesPerPage) {
-                    onPage++
-                    c = 0;
-                }
-                if (onPage == page) {
-                    count++;
-//                    log.innerHTML = log.innerHTML + "<br />" + _ob.title;
-                    title = _ob.title;
-
-                    //					if (title.length())
-                    liste += "<li><a title='" + _ob.title + "' href='" + _ob.url + "'>" + title.trunc(32) + "</a></li>";
-
-                    if (count == maxEntriesPerPage + 1) {
-                        thereIsANextPage = true;
-                        break;
-                    }
-                }
-            }
-        }
+	sanatizedObject = new Array();
+	for (i = 0; i < ob.length; i++) {
+        if (ob[i] != null) 
+			sanatizedObject.push(ob[i]);
+	}
+		
+    liste = "";		
+	// es wird eigentlich rückwärts geblättert		
+    for (i = sanatizedObject.length - ((maxEntriesPerPage * page) + maxEntriesPerPage); 
+		 i < sanatizedObject.length  && i < sanatizedObject.length - ((maxEntriesPerPage * page)); 
+		 i++) {
+		if (sanatizedObject[i] != null) {
+			_ob = JSON.parse(sanatizedObject[i]);
+        	liste = "<li><a title='" + _ob.title + "' href='" + _ob.url + "'>" + _ob.title.trunc(32) + "</a></li>" + liste;
+		}
     }
 
-
-
-	if (thereIsANextPage) {
+	if (sanatizedObject.length > (page * maxEntriesPerPage) + maxEntriesPerPage) {
         document.getElementById('next_inactive').style.display = "none";
         document.getElementById('next_active').style.display = "block";
 	} else {
@@ -65,21 +44,17 @@ function display(page) {
         document.getElementById('next_active').style.display = "none";
 	}
 
-    //alert(liste);
     document.getElementById("recentlyClosedTabsList").innerHTML = liste;
+	currentPage = page;
+}
+
+
+function log(msg) {
+	document.getElementById('log').innerHTML = document.getElementById('log').innerHTML + "<br />" + _ob.title;	
 }
 
 function init() {
-	/*
-	// all "new tab" pages have to be registered in localStorage
-	
-	chrome.tabs.getSelected(integer windowId, registerTab);
-	thisTabId = 
-	_a = JSON.parse(localStorage.allNewTabs);
-	_a[] = 
-	*/
 	display(0);	
-//	alert(localStorage.recentlyClosedTabs);
 }
 
 
@@ -96,5 +71,3 @@ function previous() {
 chrome.extension.onRequest.addListener( function(request) { 
 	window.location.reload();
 });
-
-//window.document.onFocus=alert('huhu');
